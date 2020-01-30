4f4ca4b7a5ba3a7bbdd36364e0524d963fbb460c
Ravikiran Dasari <33412537+ravikirandasari1@users.noreply.github.com>
2020-01-30 12:35:05 +0200

    Update AbstractActualIniScript.sh
    Changed DB names and Table names

--- AbstractActualIniScript.sh
+++ AbstractActualIniScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-hadoop fs -rm -r -skipTrash /Ayolanding/AbstractActualTabIncrementalOut;
+hadoop fs -rm -r -skipTrash /AbstractActualTabIncrementalOut;
 sqoop job --delete abstractActualImportJob;
 echo "    #####    Sqoop job creation    ######"
 
-sqoop job --create abstractActualImportJob -- import --options-file '/home/hdfs/sqoopimport/connectionDetails.txt' --password-file '/passwd/psw.txt' --table IAASPF.TBL_ABSTRACT_ACTUAL --append --incremental lastmodified --check-column CHANGED_AT --fields-terminated-by '\t' --target-dir '/Ayolanding/AbstractActualTabIncrementalOut' -m 1;
+sqoop job --create abstractActualImportJob -- import --options-file '/home/hdfs/sqoopimport/connectionDetails.txt' --password-file '/passwd/psw.txt' --table DB.TBL --append --incremental lastmodified --check-column CHANGED_AT --fields-terminated-by '\t' --target-dir '/AbstractActualTabIncrementalOut' -m 1;
 if [ $? -eq 0 ]; then
 echo "***************************************************************"
 echo "      Successfully Sqoop Job is created     "
@@ -24,10 +24,10 @@ else
     echo "Sqoop Import is failed"
 fi
 
-hive -e " drop table ayolanding.temp_IAASPF_TBL_ABSTRACT_ACTUAL;"
-hive -e " drop table ayolanding.IAASPF_TBL_ABSTRACT_ACTUAL;"
+hive -e " drop table DB.temp_TBL_ABSTRACT_ACTUAL;"
+hive -e " drop table DB.TBL_ABSTRACT_ACTUAL;"
 
-hive -e "create external table ayolanding.temp_IAASPF_TBL_ABSTRACT_ACTUAL(id bigint,type_discriminator string,version bigint,type_id bigint,uuid string,kind_id bigint,origin_request_id bigint,composite_comp_list_id bigint,index_comp_list int,lb_agreement_version_id bigint,role_in_actual_context_id bigint,only_actual_id bigint,changed_at string) row format delimited fields terminated by '\t' lines terminated by '\n' stored as textfile LOCATION '/Ayolanding/AbstractActualTabIncrementalOut';"
+hive -e "create external table DB.temp_TBL_ABSTRACT_ACTUAL(id bigint,type_discriminator string,version bigint,type_id bigint,uuid string,kind_id bigint,origin_request_id bigint,composite_comp_list_id bigint,index_comp_list int,lb_agreement_version_id bigint,role_in_actual_context_id bigint,only_actual_id bigint,changed_at string) row format delimited fields terminated by '\t' lines terminated by '\n' stored as textfile LOCATION '/AbstractActualTabIncrementalOut';"
 if [ $? -eq 0 ]; then
 echo "***************************************************************"
 echo "      Successfully External Table is created     "
@@ -36,7 +36,7 @@ else
     echo "External Table creation failed"
 fi
 
-hive -e "create table ayolanding.IAASPF_TBL_ABSTRACT_ACTUAL(id bigint,type_discriminator string,version bigint,type_id bigint,uuid string,kind_id bigint,origin_request_id bigint,composite_comp_list_id bigint,index_comp_list int,lb_agreement_version_id bigint,role_in_actual_context_id bigint,only_actual_id bigint,changed_at string) row format delimited fields terminated by '\t' lines terminated by '\n' stored as orcfile;"
+hive -e "create table DB.TBL_ABSTRACT_ACTUAL(id bigint,type_discriminator string,version bigint,type_id bigint,uuid string,kind_id bigint,origin_request_id bigint,composite_comp_list_id bigint,index_comp_list int,lb_agreement_version_id bigint,role_in_actual_context_id bigint,only_actual_id bigint,changed_at string) row format delimited fields terminated by '\t' lines terminated by '\n' stored as orcfile;"
 if [ $? -eq 0 ]; then
 
 echo "***************************************************************"
@@ -48,7 +48,7 @@ fi
 
 echo " starting loading records into ORC format table "
 
-hive -e "INSERT OVERWRITE TABLE ayolanding.IAASPF_TBL_ABSTRACT_ACTUAL select id,type_discriminator,version,type_id,uuid,kind_id,origin_request_id,composite_comp_list_id,index_comp_list,lb_agreement_version_id,role_in_actual_context_id,only_actual_id,changed_at from (select ROW_NUMBER() over(partition by id order by changed_at desc) rowno,* from ayolanding.temp_IAASPF_TBL_ABSTRACT_ACTUAL)a where rowno=1;"
+hive -e "INSERT OVERWRITE TABLE DB.TBL_ABSTRACT_ACTUAL select id,type_discriminator,version,type_id,uuid,kind_id,origin_request_id,composite_comp_list_id,index_comp_list,lb_agreement_version_id,role_in_actual_context_id,only_actual_id,changed_at from (select ROW_NUMBER() over(partition by id order by changed_at desc) rowno,* from DB.temp_TBL_ABSTRACT_ACTUAL)a where rowno=1;"
 if [ $? -eq 0 ]; then
 echo "***************************************************************"
 echo "      Successfully ORC Table loading is completed     "
