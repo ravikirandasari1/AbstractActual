f264028659108cb92ffedb17b2cc1ae2f364240d
Ravikiran Dasari <33412537+ravikirandasari1@users.noreply.github.com>
2020-01-30 12:44:08 +0200

    Update AbstractActualSchScript.sh
    Changed DB and TAbles names

--- AbstractActualSchScript.sh
+++ AbstractActualSchScript.sh
@@ -23,7 +23,7 @@ echo " #### starting Sqoop Impport  ####"
 
 sqoop job --exec abstractActualImportJob > /home/$USER/logging/"AbstractActlImport_log" 2>&1
   g_STATUS=$?
-  log_status $g_STATUS "IAASPF_TBL_ABSTRACT_ACTUAL table Import"
+  log_status $g_STATUS "TBL_ABSTRACT_ACTUAL table Import"
 
 if [ $? -eq 0 ]; then
 echo "***************************************************************"
@@ -35,12 +35,12 @@ fi
 
 echo " starting loading records into ORC format table "
 
-hive -e "INSERT OVERWRITE TABLE ayolanding.IAASPF_TBL_ABSTRACT_ACTUAL select id,type_discriminator,version,type_id,uuid,kind_id,origin_request_id,composite_comp_list_id,index_comp_list,lb_agreement_version_id,role_in_actual_context_id,only_actual_id,changed_at from (select ROW_NUMBER() over(partition by id order by changed_at desc) rowno,* from ayolanding.temp_IAASPF_TBL_ABSTRACT_ACTUAL)a where rowno=1;" >> /home/$USER/logging/"AbstractActlImport_log" 2>&1
+hive -e "INSERT OVERWRITE TABLE DB.TBL_ABSTRACT_ACTUAL select id,type_discriminator,version,type_id,uuid,kind_id,origin_request_id,composite_comp_list_id,index_comp_list,lb_agreement_version_id,role_in_actual_context_id,only_actual_id,changed_at from (select ROW_NUMBER() over(partition by id order by changed_at desc) rowno,* from DB.temp_TBL_ABSTRACT_ACTUAL)a where rowno=1;" >> /home/$USER/logging/"AbstractActlImport_log" 2>&1
   g_STATUS=$?
-  log_status $g_STATUS "Over Writing IAASPF_TBL_ABSTRACT_ACTUAL"
+  log_status $g_STATUS "Over Writing TBL_ABSTRACT_ACTUAL"
 
 #if [ $? -eq 0 ]; then
-#mail -s "The AbstractActual Import is successfully Completed " barry.vorster@ayoholdings.com < /home/$USER/logs/${TIMESTAMP}.success_log
+#mail -s "The AbstractActual Import is successfully Completed " ravikirandasari@gmail.com < /home/$USER/logs/${TIMESTAMP}.success_log
 #echo "***************************************************************"
 #echo "      Successfully ORC Table loading is completed     "
 #echo "***************************************************************"
@@ -48,6 +48,6 @@ hive -e "INSERT OVERWRITE TABLE ayolanding.IAASPF_TBL_ABSTRACT_ACTUAL select id,
 #    echo "ORC Table loading is failed"
 #fi
 
-hive -e "INSERT OVERWRITE TABLE ayolanding.temp_iaaspf_tbl_abstract_actual select * from ayolanding.iaaspf_tbl_abstract_actual;" >> /home/$USER/logging/"AbstractActlImport_log" 2>&1
+hive -e "INSERT OVERWRITE TABLE DB.temp_tbl_abstract_actual select * from DB.tbl_abstract_actual;" >> /home/$USER/logging/"AbstractActlImport_log" 2>&1
   g_STATUS=$?
-  log_status $g_STATUS "Over Writing TEMP_IAASPF_TBL_ABSTRACT_ACTUAL"
+  log_status $g_STATUS "Over Writing TEMP_TBL_ABSTRACT_ACTUAL"
